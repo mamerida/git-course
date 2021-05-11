b714ee2d4e859421fb45c422996a3cdb171ff815
mamerida <mamerida2018@gmail.com>
2021-05-11 15:36:49 -0400

    did modif in js test git diff and checkout

--- 01-holaMundoJs.js
+++ 01-holaMundoJs.js
@@ -1,5 +1,6 @@
 alert("Hola mundo con JS");
 alert("bienvenido al Curso");
+console.log("Hola soy una prueba de git despues checkout")
 
 console.log("Escrite esto en consola");
 console.log(88+4);//le indico para mostrar por consola
